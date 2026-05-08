307110e100f63f63f8f4f1366f8d65fbd6a50456
Ahmed Rafat <ahmedza399012@gmail.com>
2026-05-08 21:06:49 +0300

    edit login

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -3,14 +3,8 @@ import { userModel } from "../../../dataBase/models/user.model.js"
 
 export const login=async(req,res)=>{
     const {name}=req.body
-    let user=await userModel.findOne({name})
-    if(user){
-        res.status(400).json({message:"name already used"})
-
-    }else{
-        let token=name
-        await userModel.insertMany({name})
-        res.status(201).json({message:"success",token})
-    }
+    let token=name
+    await userModel.insertMany({name})
+    res.status(201).json({message:"success",token})
 
 }
